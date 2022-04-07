5846c1f71beb137900b5b234cb676464bfefd43b
Ali Khan <alikhan40310@gmail.com>
2022-04-07 12:36:03 +0500

    counter using vuex mutation methods

--- src/store/index.js
+++ src/store/index.js
@@ -2,13 +2,28 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    // all variable defined there
+    counter: 0
   },
   getters: {
   },
   mutations: {
+    // mutations are the methods that can change the state
+    increment(state) {
+      state.counter++
+    },
+    decrement(state) {
+      state.counter--
+    }
   },
   actions: {
+    // all function defined there
+  },
+  getters: {
+
   },
   modules: {
+    // each modules have there states and mutations
+
   }
 })
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+  </div>
+  <div class="counter fs-1 fw-normal">{{ $store.state.counter }}</div>
+  <div class="btns d-flex gap-2 justify-content-center align-items-center text-center">
+    <button @click="$store.commit('decrement')" class="btn btn-danger fs-2 pr-4">-</button>
+    <button @click="$store.commit('increment')" class="btn btn-primary fs-2">+</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+
+
 }
+
 </script>
