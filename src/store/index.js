d2177f9a1007e072122e1142e715d8a75a4095e1
Ali Khan <alikhan40310@gmail.com>
2022-04-11 16:42:47 +0500

    counter in vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,29 +1,46 @@
 import { createStore } from 'vuex'
-
+// import axios
+import axios from 'axios'
 export default createStore({
   state: {
     // all variable defined there
-    counter: 0
+    counter: 0,
+    colorCode: 'blue',
   },
   getters: {
   },
   mutations: {
     // mutations are the methods that can change the state
-    increment(state) {
-      state.counter++
+    increment(state, randomNumber) {
+      // state.counter++
+      state.counter += randomNumber;
     },
-    decrement(state) {
-      state.counter--
+    decrement(state, randomNumber) {
+      // state.counter--
+      state.counter -= randomNumber;
     }
   },
   actions: {
     // all function defined there
+    increment({ commit }) {
+      axios('https://www.random.org/integers/?num=1&min=1&max=6&col=1&base=10&format=plain&rnd=new').then(response => {
+        commit('increment', response.data)
+      })
+    },
+    decrement({ commit }) {
+      axios('https://www.random.org/integers/?num=1&min=1&max=6&col=1&base=10&format=plain&rnd=new').then(response => {
+        commit('decrement', response.data)
+
+      })
+    }
+
   },
   getters: {
-
+    counterSquare(state) {
+      return state.counter * state.counter
+    }
   },
   modules: {
     // each modules have there states and mutations
-
   }
 })
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,10 +2,30 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
   </div>
-  <div class="counter fs-1 fw-normal">{{ $store.state.counter }}</div>
+  <div
+    :style="{ color: $store.state.colorCode }"
+    class="counter fs-1 fw-normal"
+  >{{ $store.state.counter }}</div>
+  <div class="counter-square fs-3 mb-3">
+    {{ $store.state.counter }}
+    <sup>2</sup>
+    =
+    {{ $store.getters.counterSquare }}
+  </div>
   <div class="btns d-flex gap-2 justify-content-center align-items-center text-center">
-    <button @click="$store.commit('decrement')" class="btn btn-danger fs-2 pr-4">-</button>
-    <button @click="$store.commit('increment')" class="btn btn-primary fs-2">+</button>
+    <!-- <button @click="$store.commit('decrement')" class="btn btn-danger fs-2 pr-4">-</button>
+    <button @click="$store.commit('increment')" class="btn btn-primary fs-2">+</button>-->
+
+    <button @click="$store.dispatch('decrement')" class="btn btn-danger fs-2 pr-4">-</button>
+    <button @click="$store.dispatch('increment')" class="btn btn-primary fs-2">+</button>
+  </div>
+  <div class="d-flex col-auto justify-content-center align-items-center text-center justify-center">
+    <input
+      class="form-con col-md-2"
+      v-model="$store.state.colorCode"
+      type="text"
+      placeholder="Enter color code"
+    />
   </div>
 </template>
 
@@ -13,8 +33,16 @@
 
 export default {
   name: 'HomeView',
-
-
-}
-
+} 
 </script>
+
+<style scoped>
+.form-con {
+  border: 2px solid #ccc;
+  border-radius: 4px;
+  padding: 8px;
+  color: #333;
+  margin-top: 2rem;
+  font-size: 1.2rem;
+}
+</style>
